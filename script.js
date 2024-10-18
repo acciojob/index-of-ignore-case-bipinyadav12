a624a8aaf06f1e59291303bfb9034993cc7273b3
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-10-18 17:56:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ function indexOfIgnoreCase(s1, s2) {
  const ns2=s2.toLowerCase()
 	for ( let i=0;i<s1.length;i++){
 	
-		if (ns2.includes(ns1(i))){
+		if (ns2.includes(ns1[i]){
 			return i;}
+		else {
+			return -1
+		}
 	}
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+//const s1 = prompt("Enter s1:");
+//const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
