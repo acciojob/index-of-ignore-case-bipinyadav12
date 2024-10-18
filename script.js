4916fe82aeeb97ae37b1634e55caac1cc13e2f0c
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-10-18 18:06:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
+
 function indexOfIgnoreCase(s1, s2) {
  const ns1=s1.toLowerCase()
  const ns2=s2.toLowerCase()
 	for ( let i=0;i<s1.length;i++){
-	
-		if (ns2.includes(ns1[i]){
+		if (ns2.includes(ns1[i])){
 			return i;}
-		else {
-			return -1
-		}
 	}
 }
 
